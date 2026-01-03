bd05106424dc9ff2ce0b2035b578e462833d9740
Nikolaenko <aleshka.dev@gmail.com>
2026-01-03 15:25:23 +0200

    add proxy.ts

---
+++ proxy.ts
@@ -0,0 +1,16 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function proxy(request: NextRequest) {
+  const cookieStore = await cookies();
+
+  const token = cookieStore.get("sid")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: "/",
+};
